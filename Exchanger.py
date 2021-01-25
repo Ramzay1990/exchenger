f95239f0750783d5f59c4681d0d285ad7ac4557f
Vladyslav <melnikdmitriy43@gmail.com>
2021-01-26 00:19:36 +0200

    adding main files

---
+++ Exchanger.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template
+import requests
+
+
+app = Flask(__name__)
+app.debug = True
+
+
+def exchange(corrency_to, amount):
+    response = requests.get(f'https://api.exchangeratesapi.io/latest?symbols={corrency_to}')
+    response_json = response.json()
+    rates = response_json['rates']
+    result = str(amount * rates[corrency_to])
+    save_history(corrency_to, rates, amount, result)
+    return result
+
+
+def save_history(corrency_to, exchange_rate, amount, result):
+    file = open('history.txt', 'a')
+    file.write(f"{corrency_to},{exchange_rate[corrency_to]},{amount},{result}\n")
+    file.close()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/eur_to_usd/<int:amount>/')
+def eur_to_usd(amount):
+    usd = exchange('USD', amount)
+    return usd
+
+
+@app.route('/eur_to_gbp/<int:amount>/')
+def eur_to_gbp(amount):
+    gbp = exchange('GBP', amount)
+    return gbp
+
+
+@app.route('/eur_to_php/<int:amount>/')
+def eur_to_php(amount):
+    php = exchange('PHP', amount)
+    return php
+
+
+@app.route('/history/')
+def get_history():
+    file_ = open("history.txt")
+    file = file_.readlines()
+    return render_template("history.html", file=file)
+
+
+if __name__ == '__main__':
+    app.run()
